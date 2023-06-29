3ef7e06259a1bab25c31dba249299632178f6e05
febete <fbt.beyza@gmail.com>
2023-06-30 01:25:31 +0300

    collision detection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;    //get reference from GameInout class 
 
-    private bool isWalking;
+    private bool isWalking; 
 
 
     private void Update()
@@ -16,7 +16,54 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight ,playerRadius ,  moveDir , moveDistance);
+
+        if (!canMove)
+        {
+            //cannot move towards moveDir
+
+            // Attemp only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                // Can move only on the x
+                moveDir = moveDirX;
+
+            }
+            else
+            {
+                //cannot move only o n the x
+
+                // Attemp only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if(canMove)
+                {
+                    // Can move onlt on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //Cannot mode in any direction
+                }
+            }
+
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
+
+
+
 
         //transform.forward = moveDir;
 
