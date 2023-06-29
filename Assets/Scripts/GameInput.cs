40f4cb9c381629024c72c254aabfc4303ee5f085
febete <fbt.beyza@gmail.com>
2023-06-29 21:16:53 +0300

    Input System Refactor

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,31 +4,27 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
-   
+    PlayerInputActions playerInputActions;
+
+
+    private void Awake()
+    {
+        playerInputActions =  new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
 
-        Vector2 inputVector = new Vector2(0, 0);
+       
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
 
-        //saða sola ayný anda týklanmamasý için 
+
+        //saða sola ayný anda týklanmamasý için , birim vektör haline getiriyor
         inputVector = inputVector.normalized;
+
+        //Debug.Log(inputVector);
 
         return inputVector;
     }
